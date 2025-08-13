c73ec0e7f864947598f9197ece6615cde0345062
804-03 <slamguy14@naver.com>
2025-08-13 17:50:38 +0900

    1. 멤버데이터 뿌려주기 완성 2. member 엔티티를 DTO로 처리 완료 3.DTO 변환 시 stream의 map으로 처리함

---
+++ src/main/java/com/my/member/dto/MemberDto.java
@@ -0,0 +1,28 @@
+package com.my.member.dto;
+
+import com.my.member.entity.Member;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+
+    //엔티티를 받아서 Dto로 변환해 주는 함수
+    public static MemberDto fromMemberEntity(Member member) {
+        return new MemberDto(
+                member.getId(),
+                member.getName(),
+                member.getAge(),
+                member.getAddress()
+        );
+    }
+}
+
--- src/main/java/com/my/member/service/MemberService.java
+++ src/main/java/com/my/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.my.member.service;
 
+import com.my.member.dto.MemberDto;
 import com.my.member.entity.Member;
 import com.my.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,9 +20,27 @@ public class MemberService {
     }
 
     // 리포지토리 통해서 멤버리수투 가져오기
-    public List<Member> getAllList() {
+    public List<MemberDto> getAllList() {
         List<Member> memberList = repository.findAll();
         System.out.println(memberList);
-        return memberList;
+        //비어있는 DTO List 만들기
+//        List<MemberDto> dtoList = new ArrayList<>();
+        //Entity List를 DTO List로 변환
+//        for (int i = 0; i < memberList.size(); i++) {
+//            //리스트에 있는 엔티티를 하나씩 일어서
+//            //Dto에 다는다.
+//            MemberDto dto = new MemberDto();
+//            dto.setId((memberList.get(i).getId()));
+//            dto.setName((memberList.get(i).getName()));
+//            dto.setAge(memberList.get(i).getAge());
+//            dto.setAddress(memberList.get(i).getAddress());
+//            dtoList.add(dto);
+//        }
+        // fromMemberEntity 메서드로 작업하기
+        return  memberList
+                .stream()
+                .map(x -> MemberDto.fromMemberEntity(x))
+                .toList();
+//        return dtoList;
     }
 }
