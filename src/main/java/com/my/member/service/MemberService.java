1dce259adbf0ab8cfc25f872cf0198c63249bee8
804-03 <slamguy14@naver.com>
2025-08-13 17:01:45 +0900

    1.service 개발 2.controller 개발 - 컨트롤러에서 서비스에 전체테이터 가져오기 요청 3.showMember.html에 뿌려주기 완료

--- src/main/java/com/my/member/service/MemberService.java
+++ src/main/java/com/my/member/service/MemberService.java
@@ -1,4 +1,26 @@
-﻿package com.my.member.service;
+package com.my.member.service;
 
+import com.my.member.entity.Member;
+import com.my.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MemberService {
+    // 리표지토리를 생성자 주입방법으로 가져오기
+    private final MemberRepository repository;
+
+    public MemberService(MemberRepository memberRepository, MemberRepository repository, MemberRepository repository1, MemberRepository repository2) {
+        this.repository = repository2;
+
+
+    }
+
+    // 리포지토리 통해서 멤버리수투 가져오기
+    public List<Member> getAllList() {
+        List<Member> memberList = repository.findAll();
+        System.out.println(memberList);
+        return memberList;
+    }
 }
